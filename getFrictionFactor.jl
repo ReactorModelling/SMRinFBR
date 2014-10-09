28d800dff76f27b1a2e993dce06df500ae953da3
Anders Leirpoll <anders.leirpoll@gmail.com>
2014-10-09 13:27:51 +0200

    Laget getFrictionFactor og getReynolds

---
+++ getFrictionFactor.jl
@@ -0,0 +1,9 @@
+function getFrictionFactor(Reynolds,void)
+
+#=
+Function that calculates the Erguns equation friction factor
+=# 
+
+f=(1-void)/(void^3)*(1.75+4.2*(Reynolds^(5/6))*((1-void)/Reynolds));
+
+end
