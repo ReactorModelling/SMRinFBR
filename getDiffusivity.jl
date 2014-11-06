609f5ec30c8b565d07cb4ec25c6467abfcd2f66e
Anders Leirpoll <anders.leirpoll@gmail.com>
2014-11-06 11:17:10 +0100

    added getDiffusivity.jl

---
+++ getDiffusivity.jl
@@ -0,0 +1,16 @@
+function getDiffusivity(velocity)
+#=
+    %% Calculates the diffusivity 
+    % The Peclet numbers given in the problem formulation
+    % Input:
+    %   dParticle:  Diameter of the particles $d_\mathrm{p}$
+    %   velocity:   The axial velocity at each radial discretization $u_z$
+    %   dInner:     The inner diameter of the tubes $r_\mathrm{i}$
+    % Output:
+    %   diffusivity:    The diffusivity $D \mathrm{[m^2 s^{-1}]}$
+=#
+    
+    peNumberrd  = 8*(2-(1-2*dParticle/dInner)^2); 	# Peclet number rd
+    peNumbermr  = 1.1*peNumberrd; 				# Peclet number mr
+    diffusivity = velocity*dParticle/peNumbermr;
+end
