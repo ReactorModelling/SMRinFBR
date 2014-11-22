bde2239da7febe26a8f00c1f872b35ce310274a1
Kasper Linnestad <kasper1301@gmail.com>
2014-11-22 10:29:52 +0100

    Added comments

--- energyEquation.jl
+++ energyEquation.jl
@@ -1,16 +1,23 @@
 function energyEquation!(T, rho, uz, cp, dH, U, lambdaEff, A, b)
+    # Update A and b for all temperatures
+    # The update is done by reference
     A[1:Nr,1:Nr] = eye(Nr)
     for iZ = 2:Nz
         for iR = 1:Nr
             iGlob = iR + (iZ-1)*(Nr)
             for jZ = 1:Nz
                 for jR = 1:Nr
-                    jGlob = jR + (jZ-1)*(Nr)
+                    jGlob = jR + (jZ-1)*(Nr) # Global index j (position)
                     if iR == 1
+                        # Insert boundary condition for center symmetry
                         A[iGlob,jGlob] = Lagz[iZ,jZ]*LagAr[iR,jR]
+
                     elseif iR == Nr
+                        # Insert boundary conditon for wall
                         A[iGlob,jGlob] = lambdaEff[iGlob]*Lagz[iZ,jZ]*LagAr[iR,jR]
+
                     else
+                        # Insert energy equation: convection and radial diffusion
                         A[iGlob,jGlob] = 
                         (
                             rho[iGlob]*cp[iGlob]*uz[iGlob]
@@ -18,6 +25,7 @@ function energyEquation!(T, rho, uz, cp, dH, U, lambdaEff, A, b)
                           - lambdaEff[iGlob]*(1/r[iR]*LagAr[iR,jR]*Lagz[iZ,jZ]
                           + LagBr[iR,jR]*Lagz[iZ,jZ])
                         )
+
                     end
                 end
             end
@@ -30,8 +38,11 @@ function energyEquation!(T, rho, uz, cp, dH, U, lambdaEff, A, b)
             if iR == 1
                 continue
             elseif iR == Nr
+                # Insert heat transfer through wall
                 b[iGlob] = -U[iZ]*(T[iGlob] - Ta)
+
             else
+                # Insert heat of reaction
                 b[iGlob] = -dH[iGlob]
             end
         end
